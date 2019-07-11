8dfa4b5c434f6c4296a11d298051782efa78e827
ivanbuzyka <ivanbuzyka@gmail.com>
2019-07-11 15:14:58 +0200

    fixed path

--- DevOps/Scripts/SetARMVariables.ps1
+++ DevOps/Scripts/SetARMVariables.ps1
@@ -1,22 +1,9 @@
-$rootDirectory = $Env:BUILD_SOURCESDIRECTORY
+# Note: current directory is should be set to repository root directory in PS pipelines task
 
-Write-Host "Hello World from $Env:AGENT_NAME."
-Write-Host "My ID is $Env:AGENT_ID."
-Write-Host "AGENT_WORKFOLDER contents:"
-gci $Env:AGENT_WORKFOLDER
-Write-Host "AGENT_BUILDDIRECTORY contents:"
-gci $Env:AGENT_BUILDDIRECTORY
-Write-Host "BUILD_SOURCESDIRECTORY contents:"
-gci $Env:BUILD_SOURCESDIRECTORY
-Write-Host "Current path is:"
-Get-Location | Write-Host
-Write-Host "Over and out."
-
-
-$licenseFilePath = "$rootDirectory\DevOps\Scripts\license.xml"
+$licenseFilePath = ".\DevOps\Scripts\license.xml"
 
 # Specify the certificate file path and password if you want to deploy Sitecore 9.0 XP or XDB configurations
-$certificateFilePath = "$rootDirectory\DevOps\Scripts\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx" 
+$certificateFilePath = ".\DevOps\Scripts\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx" 
 $certificatePassword = "secret"
 $certificateBlob = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($certificateFilePath))
 $licenseFileContent = Get-Content -Raw -Encoding UTF8 -Path $licenseFilePath | Out-String
