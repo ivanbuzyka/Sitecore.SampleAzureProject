b35ba38474f9419d516caae1b4c4c5e77b57f4ed
ivanbuzyka <ivanbuzyka@gmail.com>
2019-07-11 14:06:58 +0200

    firtst test script

---
+++ DevOps/Scripts/SetARMVariables.ps1
@@ -0,0 +1,14 @@
+$rootDirectory = $Env:BUILD_SOURCESDIRECTORY
+Write-Host $Env:BUILD_SOURCESDIRECTORY
+$licenseFilePath = "$rootDirectory\Build\Scripts\license.xml"
+
+# Specify the certificate file path and password if you want to deploy Sitecore 9.0 XP or XDB configurations
+$certificateFilePath = "$rootDirectory\Build\Scripts\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx" 
+$certificatePassword = "secret"
+$certificateBlob = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($certificateFilePath))
+$licenseFileContent = Get-Content -Raw -Encoding UTF8 -Path $licenseFilePath | Out-String
+
+#$location = "westeurope"
+
+Write-Host "##vso[task.setvariable variable=certblob]$certificateBlob"
+Write-Host "##vso[task.setvariable variable=licenseContent]$licenseFileContent"
