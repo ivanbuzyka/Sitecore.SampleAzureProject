c32139e74c751bb41ce08d90c03d9d427554926d
ivanbuzyka <ivanbuzyka@gmail.com>
2019-07-11 14:52:14 +0200

    firtst fixed paths

--- DevOps/Scripts/SetARMVariables.ps1
+++ DevOps/Scripts/SetARMVariables.ps1
@@ -1,9 +1,9 @@
 $rootDirectory = $Env:BUILD_SOURCESDIRECTORY
 Write-Host $Env:BUILD_SOURCESDIRECTORY
-$licenseFilePath = "$rootDirectory\Build\Scripts\license.xml"
+$licenseFilePath = "$rootDirectory\DevOps\Scripts\license.xml"
 
 # Specify the certificate file path and password if you want to deploy Sitecore 9.0 XP or XDB configurations
-$certificateFilePath = "$rootDirectory\Build\Scripts\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx" 
+$certificateFilePath = "$rootDirectory\DevOps\Scripts\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx" 
 $certificatePassword = "secret"
 $certificateBlob = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($certificateFilePath))
 $licenseFileContent = Get-Content -Raw -Encoding UTF8 -Path $licenseFilePath | Out-String
