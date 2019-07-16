e9efb5ab047ebcdcf03f43a238288b382b1ecdfb
ivanbuzyka <ivanbuzyka@gmail.com>
2019-07-16 13:41:05 +0200

    debugging path in ps script

--- DevOps/Scripts/DeployXP.ps1
+++ DevOps/Scripts/DeployXP.ps1
@@ -1,13 +1,11 @@
 param([string] $DeploymentId = "ibu-xp-911", 
       [string] $SubscriptinId = "575f0a7c-17d6-4c66-b207-f770cbd5bbd4")
 
-$rootPath = "$Env:AGENT_RELEASEDIRECTORY\$Env:RELEASE_PRIMARYARTIFACTSOURCEALIAS"
-$rootPath1 = Get-Location
-Write-Host "Root path is: $rootPath1"
+$agentReleaseDirectory = $Env:AGENT_RELEASEDIRECTORY
+$releasePrimaryArtifactSourceAlias = $Env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
+$rootPath = "$agentReleaseDirectory\$releasePrimaryArtifactSourceAlias"
 
-Write-Host "Environment Variables: "
-Get-ChildItem $Env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
-
+Write-Host "Root path is: $rootPath"
 
 # Specify the parameters for the deployment 
 $ArmTemplateUrl = "https://emeasitecore9storageblob.blob.core.windows.net/911/arm911xp/azuredeploy.json?st=2019-06-12T09%3A35%3A00Z&se=2019-07-14T09%3A35%3A00Z&sp=rl&sv=2018-03-28&sr=c&sig=6nzXZZpyKgiRlUcT1jNCR4DKkReSk9RfcsRxoU8VOxU%3D"
