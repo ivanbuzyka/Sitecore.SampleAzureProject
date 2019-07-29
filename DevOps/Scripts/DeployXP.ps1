87254ec8126bfb7c113a485203eb306c83ec80bc
Ivan Buzyka <ibu@sitecore.net>
2019-07-29 16:27:25 +0200

    parameters changes

--- DevOps/Scripts/DeployXP.ps1
+++ DevOps/Scripts/DeployXP.ps1
@@ -63,65 +63,65 @@ if ($certificatePassword) {
 # By default this script will prompt you for your Azure credentials but you can update the script to use an Azure Service Principal instead by following the details at the link below and updating the four variables below once you are done.
 # https://azure.microsoft.com/en-us/documentation/articles/resource-group-authenticate-service-principal/
 
-$UseServicePrincipal = $false
-$TenantId = "SERVICE_PRINCIPAL_TENANT_ID"
-$ApplicationId = "SERVICE_PRINCIPAL_APPLICATION_ID"
-$ApplicationPassword = "SERVICE_PRINCIPAL_APPLICATION_PASSWORD"
-
-#endregion
-
-try 
-{
-	
-	#region Validate Resouce Group Name	
-
-	Write-Host "Validating Resource Group Name..."
-	if(!($Name -cmatch '^(?!.*--)[a-z0-9]{2}(|([a-z0-9\-]{0,37})[a-z0-9])$'))
-	{
-		Write-Error "Name should only contain lowercase letters, digits or dashes,
-					 dash cannot be used in the first two or final character,
-					 it cannot contain consecutive dashes and is limited between 2 and 40 characters in length!"
-		Break;		
-	}
-		
-	#endregion
-	
-	Write-Host "Setting Azure RM Context..."
-
- 	if($UseServicePrincipal -eq $true)
-	{
-		#region Use Service Principle
-		$secpasswd = ConvertTo-SecureString $ApplicationPassword -AsPlainText -Force
-		$mycreds = New-Object System.Management.Automation.PSCredential ($ApplicationId, $secpasswd)
-		Login-AzureRmAccount -ServicePrincipal -Tenant $TenantId -Credential $mycreds
-		
-		Set-AzureRmContext -SubscriptionID $AzureSubscriptionId -TenantId $TenantId
-		#endregion
-	}
-	else
-	{
-		#region Use Manual Login
-		try 
-		{
-			Write-Host "inside try"
-			Set-AzureRmContext -SubscriptionID $AzureSubscriptionId
-		}
-		catch 
-		{
-			Write-Host "inside catch"
-			Login-AzureRmAccount
-			Set-AzureRmContext -SubscriptionID $AzureSubscriptionId
-		}
-		#endregion		
-	}
-	
- 	Write-Host "Check if resource group already exists..."
-	$notPresent = Get-AzureRmResourceGroup -Name $Name -ev notPresent -ea 0
-	
-	if (!$notPresent) 
-	{
-		New-AzureRmResourceGroup -Name $Name -Location $location
-	}
+#$UseServicePrincipal = $false
+#$TenantId = "SERVICE_PRINCIPAL_TENANT_ID"
+#$ApplicationId = "SERVICE_PRINCIPAL_APPLICATION_ID"
+#$ApplicationPassword = "SERVICE_PRINCIPAL_APPLICATION_PASSWORD"
+#
+##endregion
+#
+#try 
+#{
+#	
+#	#region Validate Resouce Group Name	
+#
+#	Write-Host "Validating Resource Group Name..."
+#	if(!($Name -cmatch '^(?!.*--)[a-z0-9]{2}(|([a-z0-9\-]{0,37})[a-z0-9])$'))
+#	{
+#		Write-Error "Name should only contain lowercase letters, digits or dashes,
+#					 dash cannot be used in the first two or final character,
+#					 it cannot contain consecutive dashes and is limited between 2 and 40 characters in length!"
+#		Break;		
+#	}
+#		
+#	#endregion
+#	
+#	Write-Host "Setting Azure RM Context..."
+#
+# 	if($UseServicePrincipal -eq $true)
+#	{
+#		#region Use Service Principle
+#		$secpasswd = ConvertTo-SecureString $ApplicationPassword -AsPlainText -Force
+#		$mycreds = New-Object System.Management.Automation.PSCredential ($ApplicationId, $secpasswd)
+#		Login-AzureRmAccount -ServicePrincipal -Tenant $TenantId -Credential $mycreds
+#		
+#		Set-AzureRmContext -SubscriptionID $AzureSubscriptionId -TenantId $TenantId
+#		#endregion
+#	}
+#	else
+#	{
+#		#region Use Manual Login
+#		try 
+#		{
+#			Write-Host "inside try"
+#			Set-AzureRmContext -SubscriptionID $AzureSubscriptionId
+#		}
+#		catch 
+#		{
+#			Write-Host "inside catch"
+#			Login-AzureRmAccount
+#			Set-AzureRmContext -SubscriptionID $AzureSubscriptionId
+#		}
+#		#endregion		
+#	}
+#	
+# 	Write-Host "Check if resource group already exists..."
+#	$notPresent = Get-AzureRmResourceGroup -Name $Name -ev notPresent -ea 0
+#	
+#	if (!$notPresent) 
+#	{
+#		New-AzureRmResourceGroup -Name $Name -Location $location
+#	}
 	
 	# Write-Host "Starting ARM deployment..."
 	# New-AzureRmResourceGroupDeployment `
@@ -132,9 +132,9 @@ try
 	# 		# -DeploymentDebugLogLevel All -Debug -Verbose
 			
 	# Write-Host "Deployment Complete."
-}
-catch 
-{
-	Write-Error $_.Exception.Message
-	Break 
-}
+#}
+#catch 
+#{
+#	Write-Error $_.Exception.Message
+#	Break 
+#}
