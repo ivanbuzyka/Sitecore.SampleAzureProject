29631bc7c1746411a194250dd1c877200f3810ef
ivanbuzyka <ivanbuzyka@gmail.com>
2019-08-20 17:53:28 +0200

    updated sat PS

--- DevOps/Scripts/DeployXP-SAT.ps1
+++ DevOps/Scripts/DeployXP-SAT.ps1
@@ -1,21 +1,12 @@
-#param([string] $DeploymentId = "ibu-devo-911",
-#	  [string] $ResourceGroupName = "ibu-devo-911",
-#	  [string] $AzureSubscriptionId = "SUBSCRIPTION-ID",
-#	  [string] $ArmTemplateUrl = "NO-TEMPLATE-URL",
-#	# These parameter should be passed by Secure Files
-#	  [string] $LicensePath = "NO-LICENSE-FILE",
-#	  [string] $Location = "westeurope",
-#	  [string] $CertificateFilePath = "NO-CERTIFICATE-FILE",
-#	  [string] $CertificatePassword = "NO-CERTIFICATE-PASSWORD")
 param([string] $DeploymentId = "ibu-devo-911",
 	  [string] $ResourceGroupName = "ibu-devo-911",
-	  [string] $AzureSubscriptionId = "575f0a7c-17d6-4c66-b207-f770cbd5bbd4",
-	  [string] $ArmTemplateUrl = "https://emeasitecore9storageblob.blob.core.windows.net/911/arm911xpas/azuredeploy.json?st=2019-07-29T14%3A30%3A00Z&se=2020-10-16T14%3A30%3A00Z&sp=rl&sv=2018-03-28&sr=c&sig=o5N0eIAOL4DVfDyMzkko162Xq6TQku4mA3AMqmb3ZTE%3D",
+	  [string] $AzureSubscriptionId = "SUBSCRIPTION-ID",
+	  [string] $ArmTemplateUrl = "NO-TEMPLATE-URL",
 	# These parameter should be passed by Secure Files
-	  [string] $LicensePath = "C:\ibu\license\license.xml",
+	  [string] $LicensePath = "NO-LICENSE-FILE",
 	  [string] $Location = "westeurope",
-	  [string] $CertificateFilePath = "C:\projects\EA1FAC1B9F10605EEA1DDC62E6A76C15E590051A.pfx",
-	  [string] $CertificatePassword = "secret")
+	  [string] $CertificateFilePath = "NO-CERTIFICATE-FILE",
+	  [string] $CertificatePassword = "NO-CERTIFICATE-PASSWORD")
 
 Import-Module "C:\projects\DevOps\Sitecore.SampleAzureProject\DevOps\Tools\SAT-2.3.0\Sitecore.Cloud.Cmdlets.psm1"
 
@@ -24,8 +15,7 @@ $releasePrimaryArtifactSourceAlias = $Env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
 $rootPath = "$agentReleaseDirectory\$releasePrimaryArtifactSourceAlias\DevOps\Scripts"
 
 # Specify the parameters for the deployment 
-#$ArmParametersPath = "$rootPath\azuredeploy.parameters.json"
-$ArmParametersPath = "C:\projects\DevOps\Sitecore.SampleAzureProject\DevOps\Scripts\azuredeploy.parameters.json"
+$ArmParametersPath = "$rootPath\azuredeploy.parameters.json"
 
 $certificateBlob = $null
 
